c581753cdfaad79dbd6b50858fa17de1688c606d
Abdi <amohamud23@gmail.com>
2021-07-30 16:42:48 +0000

    integration test for get drivers

--- src/test/java/com/driver/controller/DriverControllerTest.java
+++ src/test/java/com/driver/controller/DriverControllerTest.java
@@ -2,8 +2,10 @@ package com.driver.controller;
 
 import com.driver.model.Driver;
 import com.driver.model.dto.DriverDto;
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -34,6 +36,18 @@ public class DriverControllerTest {
         System.out.println(response);
 
         Assertions.assertEquals(driver, response);
+
+
+    }
+
+    @Test
+    public void driverGetAll() throws JSONException {
+
+        String response = this.restTemplate.getForObject("/api/v1/drivers/", String.class );
+//        Driver response = this.restTemplate.getForObject("/api/drivers/", Driver.class);
+        System.out.println(response);
+
+        JSONAssert.assertEquals("[{id: 1}, {id: 2}]", response, true);
 
 
     }
